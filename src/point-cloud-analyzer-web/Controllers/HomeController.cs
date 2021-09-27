c6e08cb492b8b599ecb8e04af430806d54e59acb
Leon <w110056005@mail.nchu.edu.tw>
2021-09-27 14:56:29 +0800

    first commit

---
+++ src/point-cloud-analyzer-web/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using point_cloud_analyzer_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace point_cloud_analyzer_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("FileUpload")]
+        [DisableRequestSizeLimit]
+        public IActionResult Index(IFormFile file)
+        {
+            var root = System.IO.Directory.GetCurrentDirectory();
+            var upload = Path.Combine(root, "upload", file.FileName);
+            var fileName = file.FileName.Split('.')[0];
+            var output = "D:\\\\code\\potree\\output\\" + fileName;
+
+            using (Stream fileStream = new FileStream(upload, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = Path.Combine(root, "PotreeConverter", "PotreeConverter.exe"),
+                    Arguments = upload + " -o " + output + " --output-format LAZ",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            proc.WaitForExit();
+
+            string text = System.IO.File.ReadAllText(Path.Combine(root, "PotreeConverter", "template.html"));
+            text = text.Replace("[OutputFilePath]", fileName + "/cloud.js");
+            System.IO.File.WriteAllText(output + "\\..\\" + fileName + ".html", text);
+
+            System.IO.File.Delete(upload);
+
+            var redirect = "http:\\\\localhost:1234\\output\\" + fileName + ".html";
+            return Redirect(redirect);
+        }
+    }
+}
---
+++ src/point-cloud-analyzer-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace point_cloud_analyzer_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
