0db51a41d758b97458721667734deb4d572b656a
leonliu <leonliu@micron.com>
2021-09-30 10:29:54 +0800

    Add necessary libraries

--- src/point-cloud-analyzer-web/Startup.cs
+++ src/point-cloud-analyzer-web/Startup.cs
@@ -5,8 +5,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Firewall;
+using Microsoft.Extensions.FileProviders;
 
 namespace point_cloud_analyzer_web
 {
@@ -36,6 +40,31 @@ namespace point_cloud_analyzer_web
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            //var allowedIPs =
+            //    new List<IPAddress>
+            //    {
+            //        IPAddress.Parse("10.20.30.40"),
+            //        IPAddress.Parse("1.2.3.4"),
+            //        IPAddress.Parse("5.6.7.8")
+            //    };
+
+            //var allowedCIDRs =
+            //    new List<CIDRNotation>
+            //    {
+            //        CIDRNotation.Parse("110.40.88.12/28"),
+            //        CIDRNotation.Parse("88.77.99.11/8")
+            //    };
+
+            app.UseFirewall(
+                FirewallRulesEngine
+                    .DenyAllAccess()
+                //  .ExceptFromIPAddressRanges(allowedCIDRs)
+                //  .ExceptFromIPAddresses(allowedIPs)
+                .ExceptFromLocalhost()
+                .ExceptWhen(ctx=>ctx.Connection.RemoteIpAddress.ToString().StartsWith("140"))
+            );
+
             app.UseStaticFiles();
 
             app.UseRouting();
