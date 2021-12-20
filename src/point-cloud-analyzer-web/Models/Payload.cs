8949a0cad4da380f08da1d6eeac6fb9631c6880d
Leon <w110056005@mail.nchu.edu.tw>
2021-12-20 09:32:53 +0800

    Update Payload.cs

---
+++ src/point-cloud-analyzer-web/Models/Payload.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace point_cloud_analyzer_web.Models
+{
+    public class Payload
+    {
+        public string Command { get; set; }
+        public string RequestURL { get; set; }
+        public List<IFormFile> Files { get; set; } 
+    }
+}
