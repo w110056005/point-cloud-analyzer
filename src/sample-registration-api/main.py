4ddfa36469c23b5875554f66e9a9b8e62c7bc0ab
Leon <w110056005@mail.nchu.edu.tw>
2021-12-25 09:19:55 +0800

    Update main.py

--- src/sample-registration-api/main.py
+++ src/sample-registration-api/main.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_file
 from pathlib import Path
 
 app = Flask(__name__)
@@ -11,19 +11,19 @@ def registration():
     files = request.files.getlist("file")
     ext = Path(files[0].filename).suffix
     for file in files:
-        file.save("files/"+file.filename)
+        file.save(file.filename)
 
-    output = "files/"+ Path(files[0].filename).stem+"_"+Path(files[0].filename).stem+ext
+    output = Path(files[0].filename).stem+"_"+Path(files[1].filename).stem+ext
     p = subprocess.run(
         [
             'python', 'open3d_ICP_ori_v2.py', 
-            "files/"+files[0].filename, 
-            "files/"+files[1].filename, 
-            "files/"+output
+            files[0].filename, 
+            files[1].filename, 
+            output
         ]
     )
-    return  send_from_directory('files', output, as_attachment=True)
+    return send_file(output, as_attachment=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=8888)
