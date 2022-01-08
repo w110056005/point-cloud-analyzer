956cb0f6305b916f91724a4c3981e64a18dca8be
shuyinli <lakereflect@gmail.com>
2022-01-08 16:30:45 +0800

    add module list json file

--- src/point-cloud-analyzer-web/Startup.cs
+++ src/point-cloud-analyzer-web/Startup.cs
@@ -27,6 +27,7 @@ namespace point_cloud_analyzer_web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllersWithViews();
             services.AddSwaggerGen(c =>
             {
@@ -73,6 +74,8 @@ namespace point_cloud_analyzer_web
             //    .ExceptFromLocalhost()
             //    .ExceptWhen(ctx => ctx.Connection.RemoteIpAddress.ToString().StartsWith("140"))
             //);
+
+            app.UseCors(options => options.WithOrigins(Configuration.GetValue<string>("Cors:AllowedSite")).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
 
             app.UseSwagger();
 
